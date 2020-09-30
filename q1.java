e5970caca3407fa6995e2164b56f55abed9ab8f6
baishalisahoo <72142113+baishalisahoo@users.noreply.github.com>
2020-09-30 18:51:08 +0530

    Add files via upload

---
+++ q1.java
@@ -0,0 +1,23 @@
+
+public class q1
+{
+
+	public static void main(String[] args)
+	{
+		int a[]= {1,2,3,4,5};
+		int d[]=a;
+		int sum=0;
+		for(int j=0;j<=3;j++)
+		
+		System.out.println(j);
+				
+	
+			
+		
+				
+				
+		
+
+	}
+
+}
